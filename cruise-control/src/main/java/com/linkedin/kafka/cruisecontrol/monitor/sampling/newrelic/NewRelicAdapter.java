5e20e3332ee1da9bd4f6227ee4cecd36884170b2
Preetham Sura <psura@newrelic.com>
2021-07-06 14:07:45 -0700

    Enabled query retrying when query fails once

--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/monitor/sampling/newrelic/NewRelicAdapter.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/monitor/sampling/newrelic/NewRelicAdapter.java
@@ -15,6 +15,8 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,6 +24,7 @@ import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static com.linkedin.cruisecontrol.common.utils.Utils.validateNotNull;
 
@@ -30,6 +33,8 @@ import static com.linkedin.cruisecontrol.common.utils.Utils.validateNotNull;
  * and parse those outputs into usable values (NewRelicQueryResults).
  */
 class NewRelicAdapter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NewRelicAdapter.class);
+
     private static final ObjectMapper JSON = new ObjectMapper();
 
     private static final String GRAPHQL_API_PATH = "/graphql";
@@ -51,14 +56,16 @@ class NewRelicAdapter {
     /**
      * Runs our query on the NRQL endpoint. Parses the output JSON into
      * different NewRelicQueryResult values which store the metric types and
-     * metric values that we are trying to capture.
+     * metric values that we are trying to capture. Note that we retry the query
+     * once if it were to fail. If the query fails more than once, we will throw
+     * an IOException.
      * @param query - The NRQL query that we want to run to obtain our data.
      * @return - List of results from the endpoint. Each QueryResult may have
      * multiple metric types, but will only have one value associated with each metric type
      * that it contains.
      * @throws IOException
      */
-    List<NewRelicQueryResult> runQuery(String query) throws IOException {
+    List<NewRelicQueryResult> runQuery(String query) throws IOException, InterruptedException {
         // Creating the query to NRQL
         URI uri = URI.create(_newRelicEndpoint + GRAPHQL_API_PATH);
 
@@ -68,7 +75,22 @@ class NewRelicAdapter {
 
         httpPost.setEntity(buildQueryPayload(query));
 
-        // Attempting the query
+        try {
+            String responseBody = executeQuery(httpPost);
+            return processResults(responseBody);
+        } catch (IOException e) {
+            LOGGER.error("The query {} failed with error {}. Query is being retried.", query, e);
+
+            // Wait a second and then retry
+            TimeUnit.SECONDS.sleep(1);
+
+            // We retry the query
+            String responseBody = executeQuery(httpPost);
+            return processResults(responseBody);
+        }
+    }
+
+    private String executeQuery(HttpPost httpPost) throws IOException {
         try (CloseableHttpResponse response = _httpClient.execute(httpPost)) {
             int responseCode = response.getStatusLine().getStatusCode();
 
@@ -82,31 +104,35 @@ class NewRelicAdapter {
                         + "(response code = %s, response body = %s)", responseCode, responseBody));
             } else if (responseBody == null || responseBody.equals("")) {
                 throw new IOException(String.format("Received null responseBody or responseBody was empty."
-                                + " (response code = %s, response body = %s)", responseCode, responseBody));
+                        + " (response code = %s, response body = %s)", responseCode, responseBody));
             }
 
-            // We will have a null pointer exception when the query is not formatted in the way that we want it
-            // to be - This will happen most of the time because we fed an inval
-            try {
-                JsonNode responseJson = JSON.readTree(responseBody);
-                JsonNode resultsJson = responseJson.get("data").get("actor").get("account").get("nrql").get("results");
-                List<NewRelicQueryResult> results = new ArrayList<>();
+            return responseBody;
+        }
+    }
 
-                // Can be null upon invalid query or just something else going wrong on server side
-                if (resultsJson == null) {
-                    throw new IOException(String.format("ResultsJson was empty. "
-                            + "(response body = %s)", responseBody));
-                }
+    private List<NewRelicQueryResult> processResults(String responseBody) throws IOException {
+        // We will have a null pointer exception when the query is not formatted in the way that we want it
+        // to be - This will happen most of the time because we fed an invalid query
+        try {
+            JsonNode responseJson = JSON.readTree(responseBody);
+            JsonNode resultsJson = responseJson.get("data").get("actor").get("account").get("nrql").get("results");
+            List<NewRelicQueryResult> results = new ArrayList<>();
 
-                for (JsonNode resultJson : resultsJson) {
-                    results.add(new NewRelicQueryResult(resultJson));
-                }
-
-                return results;
-            } catch (NullPointerException e) {
-                throw new IOException(String.format("Response body was of invalid format and could not be"
-                        + "properly parsed. (response body = %s)", responseBody));
+            // Can be null upon invalid query or just something else going wrong on server side
+            if (resultsJson == null) {
+                throw new IOException(String.format("ResultsJson was empty. "
+                        + "(response body = %s)", responseBody));
             }
+
+            for (JsonNode resultJson : resultsJson) {
+                results.add(new NewRelicQueryResult(resultJson));
+            }
+
+            return results;
+        } catch (NullPointerException e) {
+            throw new IOException(String.format("Response body was of invalid format and could not be"
+                    + "properly parsed. (response body = %s)", responseBody));
         }
     }
 
